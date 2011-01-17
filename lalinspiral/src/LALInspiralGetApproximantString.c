1712d1126f6b2f264b0bb89f5f36738856fe8c8a
Riccardo Sturani <riccardo.sturani@ligo.org>
2011-01-17 23:26:58 +0100

    Typo corrected in XLALInspiralGetApproximantString

--- lalinspiral/src/LALInspiralGetApproximantString.c
+++ lalinspiral/src/LALInspiralGetApproximantString.c
@@ -201,11 +201,6 @@ int XLALInspiralGetApproximantString( CHAR        *output,
           "IMRPhenomB" );
       break;
 
-    case PhenSpinTaylorRD:
-      snprintf( approxString, LIGOMETA_SEARCH_MAX * sizeof(CHAR),
-          "PhenSpinTaylorRD" );
-      break;
-
     default:
       XLALPrintError("Unknown or unsupported approximant.\n");
       XLAL_ERROR( func, XLAL_EINVAL );
