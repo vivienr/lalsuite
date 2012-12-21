89558daeb131041f6050e1fef0d2305a549cd52e
Riccardo Sturani <riccardo.sturani@ligo.org>
2012-12-20 22:26:02 -0200

    Comments evicted

--- lalsimulation/test/GenerateSimulation.c
+++ lalsimulation/test/GenerateSimulation.c
@@ -334,8 +334,6 @@ int main (int argc , char **argv) {
                 difftime(time(NULL), start_time));
     if (((params->domain == GSDomain_FD) && !htilde) ||
         ((params->domain == GSDomain_TD) && (!hplus || !hcross))) {
-        if (params->domain == GSDomain_TD)
-	  printf(" Waveform T domain, vero?\n");
         XLALPrintError("Error: waveform generation failed\n");
         goto fail;
     }
